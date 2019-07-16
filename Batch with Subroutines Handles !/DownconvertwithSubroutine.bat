29ff0bb2bd44ca7c1f15828fe9f9d73ccf961a2c
Jason <34078807+j0ckinjz@users.noreply.github.com>
2019-07-16 12:33:06 -0400

    Update to ignore srt files

--- Batch with Subroutines Handles !/DownconvertwithSubroutine.bat
+++ Batch with Subroutines Handles !/DownconvertwithSubroutine.bat
@@ -15,7 +15,7 @@ Call :dequote %_srcfix% %_destfix%
 if not !_src:~-1!==\ set _src=!_src!\
 if not !_dest:~-1!==\ set _dest=!_dest!\
 ::Begin For loop
-for /R %src% %%j in (*.*) do (ENDLOCAL & set "filepath=%%j" & call "%~dp0sub.bat" %_src% %_dest%)
+for /R %src% %%j in (*.*) do if %%~xj NEQ .srt (ENDLOCAL & set "filepath=%%j" & call "%~dp0sub.bat" %_src% %_dest%)
 Pause
 cmd /k
 exit /b
